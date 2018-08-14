ec80049a4b547649c1b4e4bbd9b5778dfd2bb640
qingyang <qingyang@sinoramagroup.com>
2018-08-14 17:12:26 -0400

    index

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = new Koa();
 
 const isProduction = process.env.NODE_ENV === 'production';
 
+// log request URL:
 app.use(async (ctx, next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
     var
@@ -20,17 +21,23 @@ app.use(async (ctx, next) => {
     ctx.response.set('X-Response-Time', `${execTime}ms`);
 });
 
-
-if (!isProduction) {
+// static file support:
+if (! isProduction) {
     let staticFiles = require('./static-files');
     app.use(staticFiles('/static/', __dirname + '/static'));
 }
+
+// parse request body:
 app.use(bodyParser());
+
+// add nunjucks as view:
 app.use(templating('views', {
     noCache: !isProduction,
     watch: !isProduction
 }));
+
+// add controller:
 app.use(controller());
 
-app.listen(3000);
+app.listen(8000);
 console.log('app started at port 3000...');
